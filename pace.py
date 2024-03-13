102dfb55fdfffd2a2e883db21029128906fa3d88
Saito Kotaro <83457719+kota435@users.noreply.github.com>
2024-03-13 16:13:53 +0000

    update 表での出力

--- pace.py
+++ pace.py
@@ -1,38 +1,21 @@
+from tabulate import tabulate
 
-input_distance=input("距離を入力してください")
-input_min=input("何分？")
-input_sec=input("何秒？")
-
-min_sec=int(input_min)*60+int(input_sec)
-warukazu=int(input_distance)/100
-handred_1=min_sec/warukazu #100mのタイム
+def input_dis_pace():
+    input_distance=input("距離を入力してください")
+    input_min=input("何分")
+    input_sec=input("何秒？")
+    min_sec=int(input_min)*60+int(input_sec)
+    t_100m=min_sec*100/int(input_distance) #100mのタイム
+    distance_list=list((range(100,int(input_distance)+100,100))) #100mごとに要素を作成
+    time_list=list(range(int(t_100m),min_sec+int(t_100m),int(t_100m))) #100mごとのタイムの要素作成
+    return distance_list,time_list
 	
-distance_list=list((range(100,int(input_distance)+100,100))) #100mごとに要素を作成
-time_list=list(range(int(handred_1),int(handred_1*warukazu)+int(handred_1),int(handred_1))) #100mごとのタイムの要素作成
-	
-def pacekeisan():
-	 for output_distance,output_time in zip(distance_list,time_list): #距離出力(100mごと)
-	
-	   if output_time<60: #秒、分変換
-	     output_result=str(output_time)+'秒'
-	     print(str(output_distance)+'m'+output_result)
-		
-
-	   else:
-	     fun,byo=divmod(output_time,60)
-	     fun=int(fun)
-	     byo=int(byo)
-	     output_result=str(fun)+'分'+str(byo)+'秒'
-	     print(str(output_distance)+'m'+output_result)
-
-
-if int(input_distance)<100:
-	  print('100以上の値を入力してください')
-else:
-	  pacekeisan()
-
-
-	
-	
-	
-
+def rap_table():
+    distance,time=input_dis_pace()
+    date={
+		'distance':distance,
+        'raptime':time
+	}
+    print(tabulate(date,headers=['distance(m)','raptime(s)'],tablefmt='simple_grid'))
+     
+rap_table()
