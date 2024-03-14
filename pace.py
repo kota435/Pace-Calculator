1a9aeb3c676a1456112344de48a84d9bf005ccff
Saito Kotaro <83457719+kota435@users.noreply.github.com>
2024-03-14 13:05:31 +0000

    clean 関数を分割

--- pace.py
+++ pace.py
@@ -1,17 +1,25 @@
 from tabulate import tabulate
 
-def input_dis_pace():
-    input_distance=input("距離を入力してください")
+def input_time():
     input_min=input("何分")
     input_sec=input("何秒？")
     min_sec=int(input_min)*60+int(input_sec)
+    return min_sec
+
+def input_dis():
+    input_distance=int(input('距離を入力してください'))
+    return input_distance
+
+def create_list():
+    input_distance=input_dis()
+    min_sec=input_time()
     t_100m=min_sec*100/int(input_distance) #100mのタイム
-    distance_list=list((range(100,int(input_distance)+100,100))) #100mごとに要素を作成
     time_list=list(range(int(t_100m),min_sec+int(t_100m),int(t_100m))) #100mごとのタイムの要素作成
+    distance_list=list((range(100,int(input_distance)+100,100)))
     return distance_list,time_list
-	
+    
 def rap_table():
-    distance,time=input_dis_pace()
+    distance,time=create_list()
     date={
 		'distance':distance,
         'raptime':time
