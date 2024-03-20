f8768d0bc4a34f31678573d479425f1c6b5afb1f
Saito Kotaro <83457719+kota435@users.noreply.github.com>
2024-03-20 14:16:31 +0000

    clean 変数名変更、不必要な変数の定義を削除

--- pace.py
+++ pace.py
@@ -1,13 +1,13 @@
 from tabulate import tabulate
 import sys
 
-def convert_to_sec(a,b):
-    minutes,secounds=a,b
+def convert_to_sec(minutes,secounds):
+    #分単位から秒単位に変更
     min_sec=int(minutes)*60+int(secounds)
     return min_sec
 
-def input_dis(num):
-    c_distance=num
+def validate_distance_input(c_distance):
+    #入力された距離が100m単位ではなかった時、エラーメッセージを出し、スクリプトを終了させる
     if check_distance(c_distance):
         print('100m単位で入力して下さい')
         sys.exit()
@@ -18,25 +18,26 @@ def check_distance(num):
     ones_place=num%10
     tens_place=(num//10)%10
     
+    #一の位、十の位に0以外が入っていないか判定
     if ones_place != 0 or tens_place != 0:
         return True
     else:
         return False
 
-def create_list_distance(num):
-    distance=num
+def create_list_distance(distance):
+    #入力された距離を100mごとのリストに分割
     distance_list=list((range(100,int(distance)+100,100)))
     return distance_list
 
-def create_list_time(a,b):
-    min_sec,input_distance=a,b
-    t_100m=min_sec*100/int(input_distance) #100mのタイム
-    time_list=list(range(int(t_100m),min_sec+int(t_100m),int(t_100m))) #100mごとのタイムの要素作成
+def create_list_time(min_sec,input_distance):
+    #入力されたタイムを距離に対しての100mのタイムに
+    time_per_100m=min_sec*100/int(input_distance) 
+    time_list=list(range(int(time_per_100m),min_sec+int(time_per_100m),int(time_per_100m))) #100mごとのタイムの要素作成
     return time_list
 
-def convert_to_min(a):
-    timelist=a
+def convert_to_min(timelist):
     min_sec_list=[]
+    #与えられたタイムが60秒未満であればそのまま、60秒以上であれば繰り上げを行う
     for seconds in timelist:
         if seconds<60:
             min_sec_list.append(seconds)
@@ -46,8 +47,8 @@ def convert_to_min(a):
             min_sec_list.append(str(minutes)+"'"+str(remaing_seconds))
     return min_sec_list
 
-def sprit_table(a,b):
-    distance,time=a,b
+def output_sprit_table(distance,time):
+    #表を作成し、出力
     date={
 		'distance':distance,
         'raptime':time
@@ -59,11 +60,12 @@ def main():
     input_min=input("何分")
     input_sec=input("何秒？")
     min_sec=convert_to_sec(input_min,input_sec)
-    distance=input_dis(input_distance)
+    distance=validate_distance_input(input_distance)
     time_list=create_list_time(min_sec,distance)
     min_sec_list=convert_to_min(time_list)
     distance_list=create_list_distance(distance)
     
-    sprit_table(distance_list,min_sec_list)
+    output_sprit_table(distance_list,min_sec_list)
     
-main()
+if __name__ == '__main__':
+    main()
