c0ab9f7541fdb0eea5137a7932f9e48bfbf7ef0f
Saito Kotaro <83457719+kota435@users.noreply.github.com>
2024-03-25 02:40:54 +0000

    update 入力された距離が適切でない時の処理を追加

--- pace.py
+++ pace.py
@@ -6,13 +6,19 @@ def convert_to_sec(minutes,secounds):
     min_sec=int(minutes)*60+int(secounds)
     return min_sec
 
-def validate_distance_input(c_distance):
+def validate_distance_input():
     #入力された距離が100m単位ではなかった時、エラーメッセージを出し、スクリプトを終了させる
-    if check_distance(c_distance):
-        print('100m単位で入力して下さい')
-        sys.exit()
-    else:
-        return c_distance
+    while True:
+        try:
+            distance=int(input('100m単位で距離を入力して下さい。'))
+            if check_distance(distance):
+                print('100m単位で入力して下さい。')
+                continue
+            return distance
+            break
+        except ValueError:
+            print('半角数字を入力して下さい。')
+        
 
 def check_distance(num):
     ones_place=num%10
@@ -31,7 +37,7 @@ def create_list_distance(distance):
 
 def create_list_time(min_sec,input_distance):
     #入力されたタイムを距離に対しての100mのタイムに
-    time_per_100m=min_sec*100/int(input_distance) 
+    time_per_100m=min_sec*100/input_distance 
     time_list=list(range(int(time_per_100m),min_sec+int(time_per_100m),int(time_per_100m))) #100mごとのタイムの要素作成
     return time_list
 
@@ -56,11 +62,10 @@ def output_sprit_table(distance,time):
     print(tabulate(date,headers=['distance(m)','raptime(s)'],tablefmt='simple_grid'))
      
 def main():
-    input_distance=int(input('距離を入力してください'))
+    distance=validate_distance_input()
     input_min=input("何分")
     input_sec=input("何秒？")
     min_sec=convert_to_sec(input_min,input_sec)
-    distance=validate_distance_input(input_distance)
     time_list=create_list_time(min_sec,distance)
     min_sec_list=convert_to_min(time_list)
     distance_list=create_list_distance(distance)
