7c84c82dba87a4c6a0ca76fd53ffabd7d82bfdee
kota <kota197742@gmail.com>
2024-05-19 00:07:17 +0900

    update uiを作成

--- main.py
+++ main.py
@@ -4,20 +4,32 @@ from tkinter import ttk
 
 def calculate(*args):
     try:
-        value = int(distance.get()) 
-        output.set(value) 
+        distance_value = int(distance.get()) 
+        minutes_value = int(minutes.get())
+        secounds_value = int(secounds.get())
+        output.set(str(secounds_value)) #確認用
     except ValueError:
         pass
 
 root = Tk()
 root.title("SpritTimeCaluculate")
+
 frm = ttk.Frame(root, padding=10)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+
+ttk.Label(frm, text="SpritTimeCaluculater").grid(column=0, row=0)
 distance = StringVar()
-ttk.Entry(frm,width=5, textvariable= distance).grid(column=1,row=0)
+minutes = StringVar()
+secounds = StringVar()
+ttk.Entry(frm,textvariable= distance).grid(column=0,row=1)
+ttk.Label(frm,text="m").grid(column=1,row=1)
+ttk.Entry(frm,textvariable = minutes).grid(column=2,row=1)
+ttk.Entry(frm,textvariable = secounds).grid(column=4,row=1)
+ttk.Label(frm,text="min").grid(column=3,row=1)
+ttk.Label(frm,text="sec").grid(column=5,row=1)
+
 output = StringVar()
-ttk.Button(frm, text="Calculate", command=calculate).grid(column=1, row=1)
-ttk.Label(frm, textvariable = output).grid(column=2, row=1)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=2)
+ttk.Button(frm, text="Calculate", command=calculate).grid(column=1, row=3)
+ttk.Label(frm, textvariable = output).grid(column=2, row=2)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=2, row=3)
 root.mainloop()
