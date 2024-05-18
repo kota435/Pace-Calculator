31db02eec9a068421d62d79f226fbb481bd3ecde
kota <kota197742@gmail.com>
2024-05-19 08:16:33 +0900

    update 出力機能の修正

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ def calculate(*args):
         min_sec = minutes_value*60 + secounds_value
         distance_list = pace.create_list_distance(distance_value)
         time_list = pace.create_list_time(min_sec,distance_value)
+        min_sec_list = pace.convert_to_min(time_list)
 
-        for (distance_100m,time_100m) in zip(distance_list,time_list):
-            ttk.Label(frm2,text=distance_100m).grid(column=0)
-            ttk.Label(frm2,text=time_100m).grid(column=1)
+        for widget in frm2.winfo_children():
+            widget.destroy()
+
+        for i,(distance_100m,time_100m) in enumerate(zip(distance_list,min_sec_list)):
+            ttk.Label(frm2,text=distance_100m).grid(column=0,row=i)
+            ttk.Label(frm2,text=time_100m).grid(column=1,row=i)
     except ValueError:
         pass
 
@@ -25,7 +29,7 @@ frm2 = ttk.Frame(root,padding=10)
 frm.grid()
 frm2.grid()
 
-ttk.Label(frm, text="SpritTimeCaluculater").grid(column=0, row=0)
+ttk.Label(frm, text="Sprit Time Caluculater").grid(column=0, row=0)
 distance = StringVar()
 minutes = StringVar()
 secounds = StringVar()
